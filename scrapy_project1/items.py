1e5b83cd6763e6dcef6f126eaecbb19f20914ecd
ljk <1120391338@qq.com>
2023-02-22 23:23:01 +0800

    item spider生成

--- scrapy_project1/items.py
+++ scrapy_project1/items.py
@@ -10,3 +10,21 @@ class ScrapyProject1Item(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+
+class $classname(CrawlSpider):
+    name = "$name"
+    allowed_domains = ["$domain"]
+    start_urls = ["http://$domain/"]
+
+    rules = (Rule(LinkExtractor(allow=r"Items/"), callback="parse_item", follow=True),)
+
+    def parse_item(self, response):
+        item = {}
+        #item["domain_id"] = response.xpath('//input[@id="sid"]/@value').get()
+        #item["name"] = response.xpath('//div[@id="name"]').get()
+        #item["description"] = response.xpath('//div[@id="description"]').get()
+        return item
